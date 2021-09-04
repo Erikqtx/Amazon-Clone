e81718bcb995e0d9ac41ce0de73f78bdc78e80fa
Erikqtx <glaser-erik@web.de>
2021-09-04 16:43:36 +0200

    Fix Reducer and Product

--- amazon-clone/src/components/Product.js
+++ amazon-clone/src/components/Product.js
@@ -3,6 +3,22 @@ import "./Product.css";
 import { useStateValue } from "../StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -20,7 +36,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>In den Einkaufswagen</button>
+      <button onClick={addToBasket}>In den Einkaufswagen</button>
     </div>
   );
 }
